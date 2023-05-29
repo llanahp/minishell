07fc7165e349ad0d877dc8c992de7e363f4cd232
Mateo Pizzolo <mpizzolo@c1r15s3.42madrid.com>
2023-05-29 22:13:24 +0200

    normi en minishell.c

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mpizzolo <mpizzolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 10:41:20 by ralopez-          #+#    #+#             */
-/*   Updated: 2023/05/29 20:43:39 by mpizzolo         ###   ########.fr       */
+/*   Updated: 2023/05/29 22:11:09 by mpizzolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,4 +74,3 @@ int	main(int argc, char *argv[], char **env)
 	end_shell(&g_info);
 	return (0);
 }
-
