e1faa1706d8f5946c564c8629cbbcff605bc1e70
Raul Lopez llana <ralopez-@c1r14s3.42madrid.com>
2023-05-31 10:19:00 +0200

    cd ~,

--- vars/ft_check_vars.c
+++ vars/ft_check_vars.c
@@ -68,6 +68,20 @@ void	extend_var(char **str, t_inf *info)
 	}
 }
 
+int	expand_pox(t_list *tmp)
+{
+	int	i;
+
+	i = 0;
+	if (tmp == NULL || tmp->content == NULL)
+		return (0);
+	while (tmp->content[i] != '~')
+		i++;
+	if (tmp->content[i + 1] == '/' || tmp->content[i + 1] == '\0')
+		return (1);
+	return (0);
+}
+
 int	check_vars(t_inf *info)
 {
 	t_list	*tmp;
@@ -87,8 +101,11 @@ int	check_vars(t_inf *info)
 			&& ft_check_char_before(tmp->content, '\'', '~')
 			&& ft_check_char_before(tmp->content, '"', '~'))
 		{
-			var = get_var(info, "USER_ZDOTDIR");
-			tmp->content = replace_string(tmp->content, '~', var);
+			if (expand_pox(tmp) == 1)
+			{
+				var = get_var(info, "USER_ZDOTDIR");
+				tmp->content = replace_string(tmp->content, '~', var);
+			}
 		}	
 		tmp = tmp->next;
 	}
