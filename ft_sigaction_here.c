d3b10c6c624c01d5727b8f7075b4b3d04f7427e8
Raul Lopez llana <ralopez-@c1r2s3.42madrid.com>
2023-06-03 19:09:39 +0200

    nnormi

---
+++ ft_sigaction_here.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sigaction_here.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ralopez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/03 19:07:59 by ralopez-          #+#    #+#             */
+/*   Updated: 2023/06/03 19:08:00 by ralopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+void	signal_reset_prompt_here(int signal)
+{
+	if (signal == SIGINT)
+	{
+		(void)signal;
+		g_info.must_continue = 0;
+		g_info.last_code = 1;
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+	else if (signal == SIGQUIT)
+		SIG_IGN ;
+}
+
+
+void	set_signals_interactive_here(void)
+{
+	struct sigaction	act;
+
+	ignore_sigquit();
+	ft_memset(&act, 0, sizeof(act));
+	act.sa_handler = &signal_reset_prompt_here;
+	sigaction(SIGINT, &act, NULL);
+}
--- minishell.h
+++ minishell.h
@@ -132,6 +132,7 @@ int			exist_var(t_inf *info, char *name);
 void		set_signals_interactive(void);
 void		set_signals_interactive_here(void);
 void		set_signals_noninteractive(void);
+void		ignore_sigquit(void);
 
 /** tokenize.c */
 int			tokenize(t_inf *info, char *line);
